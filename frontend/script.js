573c4f998733684a3eaad0c84711ed47c9fa4f29
Mile Alexander <milealexander@gmail.com>
2022-04-28 09:08:26 +0200

    upgrading

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,21 @@
+const movieComponent = `
+    <h1>Movies</h1>
+`
+
 const movieCardComponent = function (title, year, summary) {
     return`
         <div class="card">
-            <h1 class="movieTitle">${title}</h1>
-            <h2 class="movieYear">${year}</h2>
-            <h3 class="movieSummary">${summary}</h3>
+            <h2 class="movieTitle">${title}</h2>
+            <h3 class="movieYear">${year}</h3>
+            <p class="movieSummary">${summary}</p>
         </div>
     `
 }
 
 const loadEvent = function () {
     const rootElement = document.getElementById("root");
+
+    rootElement.insertAdjacentHTML("beforeend", movieComponent)
 
     for (const movie of movies.cards) {
 
